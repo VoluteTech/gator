4b5fbcbabf54c4d2cde4a091d18c1b2a357b7618
VoluteTech <chewie.louveau@gmail.com>
2026-05-15 18:33:33 +0200

    feat: add postgres and register cmd

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/VoluteTech/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,10 +14,46 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 	name := cmd.Args[0]
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("could not set the current user: %w", err)
 	}
-	fmt.Println("The user name has been set.")
+
+	fmt.Println("User switched successfully!")
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: name,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create the user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set the current user: %w", err)
+	}
+	fmt.Println("User created successfully :")
+	printUser(user)
+
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID: %v\n", user.ID)
+	fmt.Printf(" * Name: %v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/VoluteTech/gator/internal/config"
+	"github.com/VoluteTech/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db *database.Queries
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("could not connect to the database: %v", err)
+	}
+	dbQueries := database.New(db)
+
 	programState := &state{
 		cfg: &cfg,
+		db: dbQueries,
 	}
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
 	if len(os.Args) < 2 {
 		log.Fatal("usage: cli <command> [args...]")
 	}
